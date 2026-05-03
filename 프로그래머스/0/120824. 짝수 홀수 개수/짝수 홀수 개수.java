0df390fde7a9d7eee0f0ba9d55fb2cbdda991e35
s-u-min <xiumin1212.g@gmail.com>
2026-05-03 21:53:16 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for (int num : num_list) {
+            answer[num % 2]++;
+        }
+
+        return answer;
+    }
+}

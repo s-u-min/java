5d27dd5f351c8bcb8a9b1ec5f435eccc08c4c542
s-u-min <xiumin1212.g@gmail.com>
2026-09-02 23:54:01 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 6.21 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String front = my_string.substring(0, s);
+        
+        int endIdx = s + overwrite_string.length();
+        String back = my_string.substring(endIdx);
+        
+        return front + overwrite_string + back;
+    }
+}
